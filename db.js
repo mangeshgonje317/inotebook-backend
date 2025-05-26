28265913ce0066144c5f3594521c1a987bcad864
Your Nckkfclsme <you@example.com>
2025-05-26 15:30:06 +0530

    chaged mongo to online

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook";
+// const mongoURI = "mongodb://localhost:27017/inotebook";
 // const mongoURI = "mongodb+srv://mangeshgonje12:SPSiT3IcMTd0vxBY@cluster0.47o5wcv.mongodb.net/";
-// const mongoURI="mongodb+srv://mangeshgonje12:Mangesh@01@cluster0.47o5wcv.mongodb.net/";
+const mongoURI= "mongodb+srv://mangeshgonje00:6V1ZySDG7PbSmtJz@cluster0.npchnw9.mongodb.net/"
 
 
 const connectToMongo = () => {
